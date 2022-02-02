b5542850c3dabd552591b2d5f8b1f47c12da8416
Jay Arredondo <jearredondo13@gmail.com>
2022-02-02 10:58:55 -0600

    finished servlets lesson

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,7 +1,7 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
