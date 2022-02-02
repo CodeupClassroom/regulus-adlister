9c22e9ee271d4cb27c0964b12c0909b96d93876d
Jay Arredondo <jearredondo13@gmail.com>
2022-02-02 11:40:51 -0600

    added query string example for getParameter

--- src/main/java/HomeServlet.java
+++ src/main/java/HomeServlet.java
@@ -15,14 +15,14 @@ public class HomeServlet extends HttpServlet {
         // Allows us to write HTML syntax in a string to present to the user.
         // Throws an IOException.
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello World!</h1>");
+//        out.println("<h1>Hello World!</h1>");
 
 
         // Get the data that is being passed along with the parameter of "email".
         String email = request.getParameter("email");
+        out.println("<h1>" + email + "</h1>");
 
         // Gives access to the session of the user. No arguments needed.
-        request.getSession();
 
         // Allows us to add a value to the request. Used to pass data from our servlet to our view.
 //        ArrayList<Products> results = new ArrayList<Products>();
@@ -30,7 +30,7 @@ public class HomeServlet extends HttpServlet {
 //        request.setAttribute("results", results);
 
         // Redirects us to a new URL pattern.
-        response.sendRedirect("/test-redirect");
+//        response.sendRedirect("/test-redirect");
 
 
     }
